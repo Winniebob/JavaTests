568d904cc7c240027ce4426776aa9960601429ea
Winniebob <131287620+Winniebob@users.noreply.github.com>
2023-10-01 19:24:16 +0300

    Delete src/seminars/third directory

--- src/seminars/third/hw/EvenOddNumber.java
+++
@@ -1,7 +0,0 @@
-package seminars.third.hw;
-
-public class EvenOddNumber {
-    public boolean isEven(int n) {
-        return n % 2 == 0;
-    }
-}
--- src/seminars/third/hw/NumberInInterval.java
+++
@@ -1,7 +0,0 @@
-package seminars.third.hw;
-
-public class NumberInInterval {
-    public boolean isNumberInInterval(int n) {
-        return n > 25 && n < 100;
-    }
-}
